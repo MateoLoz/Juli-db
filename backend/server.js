73efb1f01c53b3fa76b78472b35b5f702c94a3dc
Mateo <mateobinance1@gmail.com>
2024-07-17 12:30:49 -0300

    xdu

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+   
 })
 
 // pool.getConnection((err,con)=>{
