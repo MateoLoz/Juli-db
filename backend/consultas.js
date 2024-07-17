9d3e821aac9849018850b99fb39d681387effdb0
Mateo <mateobinance1@gmail.com>
2024-07-17 13:33:00 -0300

    xdu

---
+++ backend/consultas.js
@@ -0,0 +1,168 @@
+import {pool} from './database'
+
+
+app.get('/', (re,res)=>{
+    const sql = "SELECT * FROM usuarios";
+     pool.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+         return res.json(data);
+    })
+})
+app.post('/create',(req,res)=>{
+    
+    
+    const sql = " INSERT INTO `usuarios` ( `nombre`, `apellido`,  `email`, `membresia`, `stado`, `vencimiento`, `horas`,`entrada`, `salida`) VALUES (?);"
+    const values = [
+      req.body.nombre,
+      req.body.apellido,
+      req.body.email,
+      req.body.membresia,
+      req.body.stado,
+      req.body.vencimiento,
+      req.body.horas,
+      req.body.entrada,
+      req.body.salida,
+    ];
+    console.log(values)
+    pool.query(sql, [values], (err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Creando..");
+    })
+   
+})
+app.put('/update', (req,res)=>{
+    const sql = "UPDATE `usuarios` SET `nombre` = ?, `apellido` = ?, `email` = ?, `stado` = ?, `vencimiento` = ? , `horas` = ? WHERE `usuarios`.`id` = ?;";
+    const id = req.body.id;
+    const values = [
+        req.body.nombre,
+     req.body.apellido,
+     req.body.email,
+     req.body.stado,
+     req.body.vencimiento,
+    req.body.horas,
+    ]
+    
+     console.log(values)
+
+
+
+    pool.query(sql,[...values,id ],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json('Datos modificados!');
+    })
+})
+
+app.get('/',(re,res)=>{
+    return res.json("From backend");
+})
+
+app.delete('/', (req,res)=>{
+const sql = "DELETE FROM `usuarios` WHERE `usuarios`.`id` = ?"
+
+pool.query(sql,[req.body.id],(err,data)=>{
+    if(err) return res.json(err);
+    return res.json("eliminando..");
+})
+})
+
+app.put('/serch',(req,res)=>{
+
+const sql = "SELECT * FROM `usuarios` WHERE nombre OR apellido = ?;"
+const values = [
+    req.body.serch,
+]
+console.log(values);
+pool.query(sql,[...values],(err,data)=>{
+    if(err) return res.json(err);
+    return res.json(data);
+})
+})
+
+app.get('/Pago',(req,res)=>{
+
+    const sql = "SELECT * FROM `usuarios` WHERE usuarios.stado = 'Pago';"
+
+    pool.query(sql,(err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+    })
+
+    app.get('/Inpago',(req,res)=>{
+
+        const sql = "SELECT * FROM `usuarios` WHERE usuarios.stado = 'Inpago';"
+    
+        pool.query(sql,(err,data)=>{
+            if(err) return res.json(err);
+            return res.json(data);
+
+        })
+        })
+
+        app.get('/bono-mensual-medium',(req,res)=>{
+
+            const sql = "SELECT * FROM `usuarios` WHERE usuarios.membresia = 'bono mensual medium';"
+        
+            pool.query(sql,(err,data)=>{
+                if(err) return res.json(err);
+                return res.json(data);
+                
+            })
+            })
+
+            app.get('/bono-mensual-basico',(req,res)=>{
+
+                const sql = "SELECT * FROM `usuarios` WHERE usuarios.membresia = 'bono mensual basico';"
+            
+                pool.query(sql,(err,data)=>{
+                    if(err) return res.json(err);
+                    return res.json(data);
+                    
+                })
+                })
+
+                app.get('/bono-bicho-feo',(req,res)=>{
+
+                    const sql = "SELECT * FROM `usuarios` WHERE usuarios.membresia = 'bono bicho feo';"
+                
+                    pool.query(sql,(err,data)=>{
+                        if(err) return res.json(err);
+                        return res.json(data);
+                        
+                    })
+                    })
+
+                    app.get('/taller-intensivo',(req,res)=>{
+
+                        const sql = "SELECT * FROM `usuarios` WHERE usuarios.membresia = 'taller intensivo';"
+                    
+                        pool.query(sql,(err,data)=>{
+                            if(err) return res.json(err);
+                            return res.json(data);
+                            
+                        })
+                        })
+
+                        app.get('/bono-semanal',(req,res)=>{
+
+                            const sql = "SELECT * FROM `usuarios` WHERE usuarios.membresia = 'bono semanal';"
+                        
+                            pool.query(sql,(err,data)=>{
+                                if(err) return res.json(err);
+                                return res.json(data);
+                                
+                            })
+                            })
+    
+                            app.get('/bono-diario',(req,res)=>{
+
+                                const sql = "SELECT * FROM `usuarios` WHERE usuarios.membresia = 'bono diario';"
+                            
+                                pool.query(sql,(err,data)=>{
+                                    if(err) return res.json(err);
+                                    return res.json(data);
+                                    
+                                })
+                                })
+
+  
